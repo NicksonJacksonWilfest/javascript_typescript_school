08e822c094719ac6b9ea1f2506f367452e389c45
NicksonJacksonWilfest <devxjr.pro@gmail.com>
2025-01-12 19:36:29 +0300

    :tada: Typescript workout

---
+++ variable_and_comments.ts
@@ -0,0 +1,17 @@
+// Hello this is the first day in TypeScript review...
+// Consider static typing
+let x:number = 14;
+console.log(x)
+
+const y:string = "nick"
+console.log(y)
+
+
+// Impossible to do this in TypeScript because it is Statically typed
+
+// let x: string;
+// x = "james"; //
+// console.log(x)
+
+let nickArray: string[] = ['John', 'Juma', 'Marcus'];
+console.log(nickArray);
