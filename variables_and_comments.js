e52d4cfe214f00cf54ab39e93bbd5ff2a405f724
NicksonJacksonWilfest <devxjr.pro@gmail.com>
2025-01-12 19:02:21 +0300

    :beers: playing with variables

--- variables_and_comments.js
+++ variables_and_comments.js
@@ -1,2 +1,34 @@
+// Hello this is the first day in JavaScript review...
+
+
 var x = 2;
 console.log(x);
+
+const y = 5;
+
+
+try {
+    y = y - 1; //not allowed on constant variable
+    console.log(y);
+}
+catch (Error) {
+    console.log(Error)
+}
+
+
+
+let b = 14;
+++b
+console.log(b);
+
+let message;
+console.log(message);
+
+// console.log(null == undefined);
+
+console.log(Boolean(''));
+
+console.log(typeof 123n);
+
+let v = BigInt(123);
+console.log(typeof(x));
